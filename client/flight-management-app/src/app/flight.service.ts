7c56de26e8a38c6343e8d0bc674994ae2cb9059b
swapcom@moag.onmicrosoft.com <haimklif77@gmail.com>
2024-07-03 11:13:59 +0300

    moving the data layer and logic from flight-list-component to the flight servis

--- client/flight-management-app/src/app/flight.service.ts
+++ client/flight-management-app/src/app/flight.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Flight, FilterFlight, Airport  } from './flight.model';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Flight, FilterFlight, Airport } from './flight.model';
+import { SignalRService } from './web-socket.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
-  private apiUrl = 'https://localhost:7241/api/FlightManagement'; // Replace with your API endpoint
+  private apiUrl = 'https://localhost:7241/api/FlightManagement';
 
-  constructor(private http: HttpClient) {}
+  private flightsSubject = new BehaviorSubject<Flight[]>([]);
+  flights$ = this.flightsSubject.asObservable();
+  flickedFlightSubject = new BehaviorSubject<string>('');
+  flickedFlight$ = this.flickedFlightSubject.asObservable();
+  filter: FilterFlight = {} as FilterFlight;
 
-  getAllFlights(filter: FilterFlight): Observable<any> {
-    return this.http.post(`${this.apiUrl}/getflights`, filter);
+  constructor(private http: HttpClient, private signalRService: SignalRService) {}
+
+  getAllFlights(): void {
+    this.http.post<Flight[]>(`${this.apiUrl}/getflights`, this.filter).subscribe(
+      flights => {
+        this.flightsSubject.next(flights);
+      },
+      error => {
+        console.error('Error loading flights:', error);
+      }
+    );
   }
 
   createFlight(flight: Flight): Observable<any> {
@@ -22,7 +36,37 @@ export class FlightService {
   getAirports(): Observable<Airport[]> {
     return this.http.post<Airport[]>(`${this.apiUrl}/getairports`, {});
   }
+
+  checkFilter(flight: Flight): boolean {
+    if((this.filter.FlightNumber === '' || this.filter.FlightNumber === 'null' || this.filter.FlightNumber === flight.flightNumber)
+      && (this.filter.LandingAirport === '' || this.filter.LandingAirport === 'null' || this.filter.LandingAirport === flight.landingAirport)
+      && (this.filter.TakeoffAirport === '' || this.filter.TakeoffAirport === 'null' || this.filter.TakeoffAirport === flight.takeoffAirport)){
+        return true;
+      }
+      return false;
+  }
+
+  initSignalR(): void {
+    this.signalRService.flightUpdated$.subscribe(flight => {
+      if (flight) {
+        const flights = this.flightsSubject.getValue();
+        const index = flights.findIndex(f => f.flightNumber === flight.flightNumber);
+        if (index > -1) {
+          flights[index] = flight;
+          this.flickedFlightSubject.next(flight.flightNumber);
+        } else {
+          if (this.checkFilter(flight)) {
+            flights.push(flight);
+            this.flickedFlightSubject.next(flight.flightNumber);
+          }
+        }
+        this.flightsSubject.next([...flights]);
+      }
+    });
+  }
+
+  applyFilter(newFilter: FilterFlight): void {
+    this.filter = newFilter;
+    this.getAllFlights();
+  }
 }
-
-
-
--- client/flight-management-app/src/app/flights-list/flights-list.component.ts
+++ client/flight-management-app/src/app/flights-list/flights-list.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit, Renderer2  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FlightService } from '../flight.service';
 import { Flight, FilterFlight } from '../flight.model';
 import { EditFlightDialogComponent } from '../edit-flight-dialog/edit-flight-dialog.component';
-import { SignalRService  } from '../web-socket.service';
 import { MatTableDataSource } from '@angular/material/table';
-
 
 @Component({
   selector: 'app-flights-list',
@@ -16,46 +14,25 @@ export class FlightsListComponent implements OnInit {
   flights: MatTableDataSource<Flight> = new MatTableDataSource<Flight>();
   flickedFlight: string = '';
   displayedColumns: string[] = ['flightNumber', 'takeoffAirport', 'landingAirport', 'status',
-     'takeoffTime', 'landingTime', 'DelayTime', 'edit'];
-  filter = {} as FilterFlight;
-  constructor(private flightService: FlightService, public dialog: MatDialog,
-     private signalRService: SignalRService, private renderer: Renderer2) { }
+    'takeoffTime', 'landingTime', 'DelayTime', 'edit'];
+  filter: FilterFlight = {} as FilterFlight;
+
+  constructor(
+    private flightService: FlightService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
-    this.getFlights();
-    this.signalRService.flightUpdated$.subscribe(flight => {
-      if (flight) {
-        const index = this.flights.data.findIndex(f => f.flightNumber === flight.flightNumber);
-        if (index > -1) {
-          this.flights.data[index] = flight;
-          this.flickedFlight = this.flights.data[index].flightNumber
-          //this.triggerFlickerEffect(flight.flightNumber);
-        } else {
-          if((this.filter.FlightNumber === '' || this.filter.FlightNumber === 'null'
-             || this.filter.FlightNumber == flight.flightNumber)
-             && (this.filter.LandingAirport === '' || this.filter.LandingAirport === 'null'
-             || this.filter.LandingAirport == flight.landingAirport)
-             && (this.filter.TakeoffAirport === '' || this.filter.TakeoffAirport === 'null'
-             || this.filter.TakeoffAirport == flight.takeoffAirport)){
-            this.flights.data.push(flight);
-            this.flickedFlight = flight.flightNumber;
-          }
-        }
-        this.flights.data = [...this.flights.data];
-      }
+    this.flightService.flights$.subscribe(flights => {
+      this.flights = new MatTableDataSource(flights);
     });
 
-  }
+    this.flightService.flickedFlight$.subscribe(flightNumber => {
+      this.flickedFlight = flightNumber;
+    });
 
-  getFlights(): void {
-    this.flightService.getAllFlights(this.filter).subscribe(
-      flights => {
-        this.flights = new MatTableDataSource(flights);
-      },
-      error => {
-        console.error('Error loading flights:', error);
-      }
-    );
+    this.flightService.getAllFlights();
+    this.flightService.initSignalR();
   }
 
   editFlight(flight: Flight): void {
@@ -67,12 +44,11 @@ export class FlightsListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         this.flightService.createFlight(result).subscribe(() => {
-          this.getFlights();
+          this.flightService.getAllFlights();
         });
       }
     });
   }
-
 
   openNewFlightDialog(): void {
     const dialogRef = this.dialog.open(EditFlightDialogComponent, {
@@ -83,13 +59,13 @@ export class FlightsListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         this.flightService.createFlight(result).subscribe(() => {
-          this.getFlights();
+          this.flightService.getAllFlights();
         });
       }
     });
   }
 
   applyFilter() {
-    this.getFlights(); // Call getFlights to apply the filter
+    this.flightService.applyFilter(this.filter);
   }
 }
